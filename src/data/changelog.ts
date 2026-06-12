814e1ab8597b7aa3b4cbbb8e2789e12b85957eb5
Ilario23 <ilario.zamberlan@gmail.com>
2026-06-13 00:55:10 +0200

    refactor(changelog): clean up old entries and update changelog structure

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -43,144 +43,23 @@ export const changelogData: ChangeLogEntry[] = [
     ],
   },
   {
-    version: '2.0.2',
-    date: '2026-06-03',
-    changes: [
-      {type: 'refactor', description: 'changelog_v2_0_2_0'},
-      {type: 'refactor', description: 'changelog_v2_0_2_1'},
-    ],
-  },
-  {
-    version: '2.0.1',
-    date: '2026-06-03',
-    changes: [
-      {type: 'refactor', description: 'changelog_v2_0_1_0'},
-      {type: 'feat', description: 'changelog_v2_0_1_1'},
-      {type: 'refactor', description: 'changelog_v2_0_1_2'},
-      {type: 'fix', description: 'changelog_v2_0_1_3'},
-    ],
-  },
-  {
     version: '2.0.0',
     date: '2026-06-03',
     changes: [
-      {type: 'feat', description: 'changelog_v2_0_0_0'},
-      {type: 'feat', description: 'changelog_v2_0_0_1'},
-      {type: 'feat', description: 'changelog_v2_0_0_2'},
-      {type: 'feat', description: 'changelog_v2_0_0_3'},
-      {type: 'feat', description: 'changelog_v2_0_0_4'},
-      {type: 'feat', description: 'changelog_v2_0_0_5'},
-      {type: 'refactor', description: 'changelog_v2_0_0_6'},
-      {type: 'fix', description: 'changelog_v2_0_0_7'},
-    ],
-  },
-  {
-    version: '1.0.12',
-    date: '2026-05-06',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_12_0'},
-      {type: 'fix', description: 'changelog_v1_0_12_1'},
-    ],
-  },
-  {
-    version: '1.0.11',
-    date: '2026-05-06',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_11_0'},
-      {type: 'feat', description: 'changelog_v1_0_11_1'},
-      {type: 'feat', description: 'changelog_v1_0_11_2'},
-      {type: 'fix', description: 'changelog_v1_0_11_3'},
-    ],
-  },
-  {
-    version: '1.0.10',
-    date: '2026-05-03',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_10_0'},
-      {type: 'fix', description: 'changelog_v1_0_10_1'},
-      {type: 'fix', description: 'changelog_v1_0_10_2'},
-    ],
-  },
-  {
-    version: '1.0.9',
-    date: '2026-03-23',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_9_0'},
-      {type: 'feat', description: 'changelog_v1_0_9_1'},
-    ],
-  },
-  {
-    version: '1.0.8',
-    date: '2026-02-24',
-    changes: [{type: 'fix', description: 'changelog_v1_0_8_0'}],
-  },
-  {
-    version: '1.0.7',
-    date: '2026-01-22',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_7_0'},
-      {type: 'feat', description: 'changelog_v1_0_7_1'},
-      {type: 'fix', description: 'changelog_v1_0_7_2'},
-    ],
-  },
-  {
-    version: '1.0.6',
-    date: '2026-01-19',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_6_0'},
-      {type: 'feat', description: 'changelog_v1_0_6_1'},
-      {type: 'feat', description: 'changelog_v1_0_6_2'},
-    ],
-  },
-  {
-    version: '1.0.5',
-    date: '2026-01-16',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_5_0'},
-      {type: 'feat', description: 'changelog_v1_0_5_1'},
-      {type: 'refactor', description: 'changelog_v1_0_5_2'},
-      {type: 'refactor', description: 'changelog_v1_0_5_3'},
-      {type: 'fix', description: 'changelog_v1_0_5_4'},
-      {type: 'chore', description: 'changelog_v1_0_5_5'},
-    ],
-  },
-  {
-    version: '1.0.4',
-    date: '2026-01-14',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_4_0'},
-      {type: 'fix', description: 'changelog_v1_0_4_1'},
-    ],
-  },
-  {
-    version: '1.0.3',
-    date: '2026-01-10',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_3_0'},
-      {type: 'feat', description: 'changelog_v1_0_3_1'},
-    ],
-  },
-  {
-    version: '1.0.2',
-    date: '2026-01-10',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_2_0'},
-      {type: 'feat', description: 'changelog_v1_0_2_1'},
-      {type: 'fix', description: 'changelog_v1_0_2_2'},
-      {type: 'refactor', description: 'changelog_v1_0_2_3'},
-    ],
-  },
-  {
-    version: '1.0.1',
-    date: '2026-01-09',
-    changes: [
-      {type: 'feat', description: 'changelog_v1_0_1_0'},
-      {type: 'feat', description: 'changelog_v1_0_1_1'},
+      {type: 'feat', description: 'changelog_v2_0_x_0'},
+      {type: 'feat', description: 'changelog_v2_0_x_1'},
+      {type: 'refactor', description: 'changelog_v2_0_x_2'},
+      {type: 'fix', description: 'changelog_v2_0_x_3'},
     ],
   },
   {
     version: '1.0.0',
     date: '2026-01-08',
-    changes: [{type: 'feat', description: 'changelog_v1_0_0_0'}],
+    changes: [
+      {type: 'feat', description: 'changelog_v1_x_0'},
+      {type: 'feat', description: 'changelog_v1_x_1'},
+      {type: 'feat', description: 'changelog_v1_x_2'},
+      {type: 'fix', description: 'changelog_v1_x_3'},
+    ],
   },
 ];
